69670dc1232ab8a905c5f78266f1af04f6f620dd
Roman Radchenko <nodenwwsfww@gmail.com>
2020-08-23 10:49:11 +0300

    del keys

--- keys/index.js
+++ keys/index.js
@@ -1,7 +1,7 @@
 module.exports = {
-    MONGODB_URI: `mongodb+srv://nwwsfww:4wLwiA26Gs9Eoqnl@cluster0.xe1qk.mongodb.net/shop`,
-    SESSION_SECRET: '3g23g23g2fffs2211kutrehorwegtr4',
-    SENDGRID_API_KEY: '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',
-    EMAIL_FROM: 'nodejs@email.ru',
-    BASE_URL: 'http://localhost:3000'
+    MONGODB_URI: ``,
+    SESSION_SECRET: '',
+    SENDGRID_API_KEY: '',
+    EMAIL_FROM: '',
+    BASE_URL: ''
 }
